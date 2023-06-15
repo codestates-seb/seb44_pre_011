a43d4e0258d35c0e9b6ac05bebf7c97d548bb435
park-tae-yang <pxodid2000@gmail.com>
2023-06-15 15:25:44 +0900

    commit

---
+++ src/main/java/com/district11/stackoverflow/StackoverflowApplication.java
@@ -0,0 +1,13 @@
+package com.district11.stackoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackoverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackoverflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/district11/stackoverflow/StackoverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.district11.stackoverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackoverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
