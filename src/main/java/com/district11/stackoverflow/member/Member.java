bb04761a1cb058a3cb28451081699ae90c71800f
Parkjaeseong94 <wotjddml159@gmail.com>
2023-06-15 17:47:32 +0900

    first commit

--- src/main/java/com/district11/stackoverflow/member/Member.java
+++ src/main/java/com/district11/stackoverflow/member/Member.java
@@ -1,11 +1,11 @@
-//package com.district11.stackoverflow.member;
-//
-//
-//
-//import javax.persistence.Entity;
-//
-//@Entity
-//public class Member {
-//    public String name;
-//
-//}
+package com.district11.stackoverflow.member;
+
+
+
+import javax.persistence.Entity;
+
+@Entity
+public class Member {
+    public String name;
+
+}
