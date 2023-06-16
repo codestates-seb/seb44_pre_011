6e6d07a75d41026f4839bae83fd31198b20062e0
Parkjaeseong94 <wotjddml159@gmail.com>
2023-06-16 09:33:07 +0900

    excepton directory

--- src/main/java/com/district11/stackoverflow/question/controller/QuestionController.java
+++ src/main/java/com/district11/stackoverflow/question/controller/QuestionController.java
@@ -1,4 +1,21 @@
 package com.district11.stackoverflow.question.controller;
 
+import com.district11.stackoverflow.question.dto.QuestionPostDto;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/questions")
+@Validated
 public class QuestionController {
+
+    @PostMapping("/ask")
+    public ResponseEntity postQuestion(@Valid @RequestBody QuestionPostDto requestBody) {
+        return null;
+    }
 }
--- src/main/java/com/district11/stackoverflow/question/entity/Question.java
+++ src/main/java/com/district11/stackoverflow/question/entity/Question.java
@@ -1,14 +1,21 @@
 package com.district11.stackoverflow.question.entity;
+import com.district11.stackoverflow.audit.Auditable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
-public class Question {
-
-    public Long questionId;
-
-    public String content;
-
-
-
+@Getter
+@Setter
+@NoArgsConstructor
+public class Question extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long questionId;
+    @Column
+    private String title;
+    @Column
+    private String content;
 }
