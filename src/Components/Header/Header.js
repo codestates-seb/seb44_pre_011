0deae3930a5ee71b04b827790ccff34c0f637c0b
yejinzz <yejin1320@naver.com>
2023-06-16 19:41:06 +0900

    Link 적용

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,21 +1,34 @@
 import Style from "./Header.module.css";
-// import SearchIcon from "@mui/icons-material/Search";
+import { Link } from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
 import Button from "@mui/material/Button";
 
 const Header = () => {
   return (
     <header className={Style.headerContainer}>
-      <img
-        className={Style.logo}
-        src={`${process.env.PUBLIC_URL}/img/logo-stackoverflow.png`}
-        alt="logo"
-      ></img>
+      <Link to="/">
+        <img
+          className={Style.logo}
+          src={`${process.env.PUBLIC_URL}/img/logo-stackoverflow.png`}
+          alt="logo"
+        ></img>
+      </Link>
       <span className={Style.SearchBarContainer}>
-        {/* <SearchIcon className={Style.searchIcon} /> */}
+        <SearchIcon className={Style.searchIcon} />
         <input className={Style.searchBar} type="text" />
       </span>
-      <Button variant="contained">Log IN</Button>
-      <Button variant="contained">Sign Up</Button>
+      <span className={Style.buttonBox}>
+        <Link to="/login">
+          <Button className={Style.button} variant="contained">
+            Log IN
+          </Button>
+        </Link>
+        <Link>
+          <Button className={Style.button} variant="contained">
+            Sign Up
+          </Button>
+        </Link>
+      </span>
     </header>
   );
 };
