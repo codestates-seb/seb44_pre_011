c86091a51172fa7352d9127b6c981f3658ff5b26
오승찬 <oseungchan@oseungchan-ui-MacBookAir.local>
2023-06-16 16:26:39 +0900

    aside(완) -feat: router 경로는 설정 전

--- src/Components/Aside/Aside.js
+++ src/Components/Aside/Aside.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import { Typography } from "@mui/material";
+import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
+import LocalOfferIcon from "@mui/icons-material/LocalOffer";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import SettingsIcon from "@mui/icons-material/Settings";
+import { NavLink } from "react-router-dom";
+import "./aside.css";
 
 const Aside = () => {
+  // 경로 설정 필요
   return (
-    <div>Aside</div>
-  )
-}
+    <div id="Btns_aside">
+      <NavLink
+        // to="/"
+        className="asideButton"
+        activeclassname="asideButton active"
+      >
+        <QuestionMarkIcon className="icon" />
+        <Typography>Questions</Typography>
+      </NavLink>
+      <NavLink
+        to="/tags"
+        className="asideButton "
+        activeclassname="asideButton active"
+      >
+        <LocalOfferIcon className="icon" />
+        <Typography>Tags</Typography>
+      </NavLink>
+      <NavLink
+        to="/users"
+        className="asideButton "
+        activeclassname="asideButton active"
+      >
+        <PeopleAltIcon className="icon" />
+        <Typography>Users</Typography>
+      </NavLink>
+      <NavLink
+        to="about"
+        className="asideButton "
+        activeclassname="asideButton active"
+      >
+        <SettingsIcon className="icon" />
+        <Typography>About</Typography>
+      </NavLink>
+    </div>
+  );
+};
 
-export default Aside
+export default Aside;
