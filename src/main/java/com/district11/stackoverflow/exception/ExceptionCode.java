fbabf47ba17c060c42f92b2d79f7ddf269195282
Parkjaeseong94 <wotjddml159@gmail.com>
2023-06-16 13:26:51 +0900

    Question CRUD 기능 추가

--- src/main/java/com/district11/stackoverflow/exception/ExceptionCode.java
+++ src/main/java/com/district11/stackoverflow/exception/ExceptionCode.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 public enum ExceptionCode {
     MEMBER_NOT_FOUND(404, "Member not found"),
     MEMBER_EXISTS(409, "Member exists"),
-    ANSWER_NOT_FOUND(404, "Answer not found");
+    ANSWER_NOT_FOUND(404, "Answer not found"),
+    QUESTION_NOT_FOUND(404, "Question not found");
 
     @Getter
     private int status;
--- src/main/java/com/district11/stackoverflow/question/service/QuestionService.java
+++ src/main/java/com/district11/stackoverflow/question/service/QuestionService.java
@@ -1,8 +1,16 @@
 package com.district11.stackoverflow.question.service;
 
+import com.district11.stackoverflow.exception.BusinessLogicException;
+import com.district11.stackoverflow.exception.ExceptionCode;
 import com.district11.stackoverflow.question.entity.Question;
 import com.district11.stackoverflow.question.repository.QuestionRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+@Service
 public class QuestionService {
     private QuestionRepository questionRepository;
 
@@ -14,5 +22,29 @@ public class QuestionService {
         return questionRepository.save(question);
     }
 
+    public Question updateQuestion(Question question){
+        Question findQuestion = findQuestion(question.getQuestionId());
+        Optional.ofNullable(question.getTitle())
+                .ifPresent(findQuestion::setTitle);
+        Optional.ofNullable(question.getContent())
+                .ifPresent(findQuestion::setContent);
+
+        return findQuestion;
+    }
+    public void deleteQuestion(long questionId){
+        Question question = findQuestion(questionId);
+        questionRepository.deleteById(questionId);
+    }
+
+    public Question findQuestion(long questionId){
+        Optional<Question> optionalQuestion = questionRepository.findById(questionId);
+        Question findQuestion = optionalQuestion.orElseThrow(()->new BusinessLogicException(ExceptionCode.QUESTION_NOT_FOUND));
+
+        return findQuestion;
+    }
+
+    public Page<Question> findQuestions(int page, int size){
+        return questionRepository.findAll(PageRequest.of(page,size, Sort.by("questionId").descending()));
+    }
 
 }
