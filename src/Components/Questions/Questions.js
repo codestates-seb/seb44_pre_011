e6f0031d5b59607c0f4fa1c168ca01fb1909a5ae
weed7968 <tlstjdcjf980809@gmail.com>
2023-06-16 17:22:12 +0900

    Questions 컴포넌트

--- src/Components/Questions/Questions.js
+++ src/Components/Questions/Questions.js
@@ -1,7 +1,28 @@
 import React from "react";
+import style from "./Questions.module.css";
 
 const Questions = () => {
-  return <div>Questions</div>;
+  return (
+    <div id={style.container}>
+      <h3>What is the fastest way to get the value of π?</h3>
+      <div id={style.text}>
+        'm looking for the fastest way to obtain the value of π, as a personal
+        challenge. More specifically, I'm using ways that don't involve using
+        #define constants like M_PI, or hard-coding the number in.
+      </div>
+      <div id={style.bottom}>
+        <div id={style.taglist}>
+          <div className={style.tag}>tag1</div>
+          <div className={style.tag}>tag2</div>
+          <div className={style.tag}>tag3</div>
+        </div>
+        <div id={style.userInfo}>
+          <img src={process.env.PUBLIC_URL + "/img/test_img.jpg"} alt="test" />
+          Username 2023/06/16 at 12:00
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Questions;
