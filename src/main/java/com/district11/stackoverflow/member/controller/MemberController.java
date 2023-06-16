60ee76d404b1b6d3e880f502dc6e3f95ef352552
park-tae-yang <pxodid2000@gmail.com>
2023-06-16 17:19:31 +0900

    member basic

--- src/main/java/com/district11/stackoverflow/member/controller/MemberController.java
+++ src/main/java/com/district11/stackoverflow/member/controller/MemberController.java
@@ -55,7 +55,7 @@ public class MemberController {
     @GetMapping
     public ResponseEntity getMembers(@Positive @RequestParam int page,
                                      @Positive @RequestParam int size) {
-        Page<Member> pageMembers = memberService.findMembers(page, size);
+        Page<Member> pageMembers = memberService.findMembers(page-1, size);
         List<Member> members = pageMembers.getContent();
         return new ResponseEntity(
                 new MultiResponseDto(memberMapper.membersToMemberResponseDtos(members),pageMembers),HttpStatus.OK
