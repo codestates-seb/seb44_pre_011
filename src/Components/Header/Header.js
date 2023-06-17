da9f0b3970b1eda9b524b028a0785f743ad39fac
yejinzz <yejin1320@naver.com>
2023-06-17 11:57:44 +0900

    menu dropdown 구현

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,40 +1,55 @@
 import Style from "./Header.module.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import Button from "@mui/material/Button";
 import MenuIcon from "@mui/icons-material/Menu";
+import Aside from "../Aside/Aside";
 
 const Header = () => {
-  return (
-      <header className={Style.headerContainer}>
-      {(window.location.pathname === "/login" || window.location.pathname === "/signup") && (
-          <MenuIcon />
-        )}
-        <Link to="/">
-          <img
-            className={Style.logo}
-            src={`${process.env.PUBLIC_URL}/img/logo-stackoverflow.png`}
-            alt="logo"
-          ></img>
-        </Link>
-        <span className={Style.SearchBarContainer}>
-          <SearchIcon className={Style.searchIcon} />
-          <input className={Style.searchBar} type="text" />
-        </span>
-        <span className={Style.buttonBox}>
-          <Link to="/login">
-            <Button className={Style.button} variant="contained">
-              Log IN
-            </Button>
-          </Link>
+  const [view, setView] = useState(false);
 
-          <Link>
-            <Button className={Style.button} variant="contained">
-              Sign Up
-            </Button>
-          </Link>
-        </span>
-      </header>
+  const toggleDropdown = () => {
+    setView(!view);
+  };
+
+  return (
+    <header className={Style.headerContainer}>
+      <div className={Style.Container}>
+        {(window.location.pathname === "/login" ||
+          window.location.pathname === "/signup") && (
+          <MenuIcon onClick={toggleDropdown} />
+        )}
+        <div className={Style.dropdownContainer}>
+          {view && <Aside className={Style.dropdown} />}
+        </div>
+      </div>
+
+      <Link to="/">
+        <img
+          className={Style.logo}
+          src={`${process.env.PUBLIC_URL}/img/logo-stackoverflow.png`}
+          alt="logo"
+        ></img>
+      </Link>
+      <span className={Style.SearchBarContainer}>
+        <SearchIcon className={Style.searchIcon} />
+        <input className={Style.searchBar} type="text" />
+      </span>
+      <span className={Style.buttonBox}>
+        <Link to="/login">
+          <Button className={Style.button} variant="contained">
+            Log IN
+          </Button>
+        </Link>
+
+        <Link>
+          <Button className={Style.button} variant="contained">
+            Sign Up
+          </Button>
+        </Link>
+      </span>
+    </header>
   );
 };
 
