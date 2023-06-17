6441716bf187f4b74038cac529556ce989a24def
yejinzz <yejin1320@naver.com>
2023-06-17 23:16:57 +0900

    header profile 구현

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,10 +7,12 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Aside from "../Aside/Aside";
 
 const Header = () => {
-  const [view, setView] = useState(false);
+  const [menuView, setMenuView] = useState(false);
+
+  const [isLogin] = useState(false);
 
   const toggleDropdown = () => {
-    setView(!view);
+    setMenuView(!menuView);
   };
 
   return (
@@ -20,11 +22,11 @@ const Header = () => {
           window.location.pathname === "/signup") && (
           <MenuIcon
             onClick={toggleDropdown}
-            sx={{ display: "block", height: "100%", width: "25px" }}
+            sx={{ display: "flex", height: "100%", width: "25px" }}
           />
         )}
         <div className={Style.dropdownContainer}>
-          {view && <Aside className={Style.dropdown} />}
+          {menuView && <Aside className={Style.dropdown} />}
         </div>
       </div>
 
@@ -42,33 +44,52 @@ const Header = () => {
         />
         <input className={Style.searchBar} type="text" />
       </span>
-      <span className={Style.buttonBox}>
-        <Link to="/login">
-          <Button
-            className={Style.button}
-            variant="contained"
-            sx={{
-              fontSize: "14px",
-              width: "93px",
-              height: "30px",
-            }}
-          >
-            Log IN
-          </Button>
-        </Link>
 
-        <Link>
-          <Button
-            className={Style.button}
-            variant="contained"
-            sx={{ fontSize: "14px", width: "93px", height: "30px" }}
-          >
-            Sign Up
-          </Button>
-        </Link>
-      </span>
+      {isLogin ? <UserInfo /> : <LinkButton />}
     </header>
   );
 };
 
 export default Header;
+
+const UserInfo = () => {
+  return (
+    <div className={Style.profileContainer}>
+      <img
+        className={Style.defaultPicture}
+        src={`${process.env.PUBLIC_URL}/img/test_img.jpg`}
+        alt="default_picture"
+      ></img>
+      <div>display name</div>
+    </div>
+  );
+};
+
+const LinkButton = () => {
+  return (
+    <span className={Style.buttonContainer}>
+      <Link to="/login">
+        <Button
+          className={Style.button}
+          variant="contained"
+          sx={{
+            fontSize: "14px",
+            width: "93px",
+            height: "30px",
+          }}
+        >
+          Log IN
+        </Button>
+      </Link>
+      <Link to="/signup">
+        <Button
+          className={Style.button}
+          variant="contained"
+          sx={{ fontSize: 14, width: "93px", height: "30px" }}
+        >
+          Sign Up
+        </Button>
+      </Link>
+    </span>
+  );
+};
