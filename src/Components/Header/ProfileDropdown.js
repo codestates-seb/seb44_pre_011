39e27d55b009facca8dfe5e39d4522931d975930
yejinzz <yejin1320@naver.com>
2023-06-19 01:04:57 +0900

    css 수정

--- src/Components/Header/ProfileDropdown.js
+++ src/Components/Header/ProfileDropdown.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Style from "./ProfileDropdown.module.css";
 
-const deactiveStyle = {
-    textDecoration: 'none',
-    color: 'black',
-}
+// const deactiveStyle = {
+//   textDecoration: "none",
+//   color: "black",
+// };
 
-export default function DropMenu () {
-
-    return (
-      <>
-        <div>
-            <div>
-                <NavLink to='/mypage' style={deactiveStyle} >My page</NavLink>
-            </div>
-            <div>
-                <NavLink to='/editprofile' style={deactiveStyle}>Edit Profile</NavLink>
-            </div>
-            <div>
-                <NavLink to='/' style={deactiveStyle}>Logout</NavLink>
-            </div>
+export default function DropMenu() {
+  return (
+    <>
+      <div>
+        <div className={Style.menuItem}>
+          <NavLink to="/mypage">My page</NavLink>
         </div>
-      </>
-    );
-  }
+        <div>
+          <NavLink to="/editprofile">Edit Profile</NavLink>
+        </div>
+        <div>
+          <NavLink to="/">Logout</NavLink>
+        </div>
+      </div>
+    </>
+  );
+}
