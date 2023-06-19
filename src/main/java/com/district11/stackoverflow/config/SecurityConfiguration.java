32f43adf2020899ee651be94c6f78d08fb9086eb
park-tae-yang <pxodid2000@gmail.com>
2023-06-19 09:20:12 +0900

    auth base

---
+++ src/main/java/com/district11/stackoverflow/config/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.district11.stackoverflow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public UserDetailsManager userDetailsService() {
+        UserDetails userDetails =
+                User.withDefaultPasswordEncoder()
+                        .username("abc@gmail.com")
+                        .password("abc")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+}
