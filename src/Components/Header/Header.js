161e26c48680337221eebaf2e3b96bcb79f61252
yejinzz <yejin1320@naver.com>
2023-06-19 10:37:55 +0900

    Header css 수정

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -10,7 +10,7 @@ import ProfileDropdown from "./ProfileDropdown";
 const Header = () => {
   const [menuView, setMenuView] = useState(false);
 
-  const [isLogin] = useState(true);
+  const [isLogin] = useState(false);
 
   const toggleDropdown = () => {
     setMenuView(!menuView);
@@ -43,7 +43,11 @@ const Header = () => {
           className={Style.searchIcon}
           sx={{ width: "30px", height: "100%" }}
         />
-        <input className={Style.searchBar} type="text" />
+        <input
+          className={Style.searchBar}
+          placeholder="Search..."
+          type="text"
+        />
       </span>
 
       {isLogin ? <UserInfo /> : <LinkButton />}
@@ -69,7 +73,7 @@ const UserInfo = () => {
         onClick={Dropdown}
       ></img>
       <div className={Style.ProfileDropdowncontainer}>
-          {menuView && <ProfileDropdown className={Style.MenuItem} />}
+        {menuView && <ProfileDropdown className={Style.MenuItem} />}
       </div>
       <div>display name</div>
     </div>
@@ -90,7 +94,9 @@ const LinkButton = () => {
             margin: "10px",
             backgroundColor: "#e3ecf3",
           }}
-        > Login
+        >
+          {" "}
+          Login
         </Button>
       </Link>
       <Link to="/signup">
