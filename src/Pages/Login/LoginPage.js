4a4fcbdd067b81fb8c99b33ef9dfe1a885e16d43
shun-is-me <rhkrtlgus@naver.com>
2023-06-20 11:22:35 +0900

    수정1

--- src/Pages/Login/LoginPage.js
+++ src/Pages/Login/LoginPage.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
 import Oauth from "../../Components/Oauth/Oauth";
+import style from "./LoginPage.module.css"
 
 const LoginPage = () => {
   return (
-    <div>
+    <>
       <Header />
-      <Oauth />
-    </div>
+      <div className={style.AllContainer}>
+        <img src={`${process.env.PUBLIC_URL}/img/StackOverFlow_logo.svg`} className={style.logoimg}/>
+        <Oauth />
+      </div>
+    </>
   );
 };
 
