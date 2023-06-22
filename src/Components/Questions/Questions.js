dbd0f9f770177272c9c8be81dd26481c15ee957f
오승찬 <oseungchan@oseungchan-ui-MacBookAir.local>
2023-06-22 11:35:21 +0900

    users 페이지 완, userProfile 수정

--- src/Components/Questions/Questions.js
+++ src/Components/Questions/Questions.js
@@ -2,11 +2,15 @@ import React from "react";
 import style from "./Questions.module.css";
 import { useNavigate } from "react-router";
 
-const Questions = ({ key, title, content }) => {
+const Questions = ({ questionId, title, content }) => {
+  const navigate = useNavigate();
+  const ReadQuestios = () => {
+    navigate(`/questions/read?id=${questionId}`);
+  };
   return (
-    <div id={style.container}>
-      <h3>title</h3>
-      <div id={style.text}>content</div>
+    <div id={style.container} onClick={ReadQuestios}>
+      <h3>{title}</h3>
+      <div id={style.text}>{content}</div>
       <div id={style.bottom}>
         <div id={style.taglist}>
           <div className={style.tag}>tag1</div>
@@ -14,7 +18,10 @@ const Questions = ({ key, title, content }) => {
           <div className={style.tag}>tag3</div>
         </div>
         <div id={style.userInfo}>
-          <img src={process.env.PUBLIC_URL + "/img/test_img.jpg"} alt={key} />
+          <img
+            src={process.env.PUBLIC_URL + "/img/test_img.jpg"}
+            alt={questionId}
+          />
           Username 2023/06/16 at 12:00
         </div>
       </div>
