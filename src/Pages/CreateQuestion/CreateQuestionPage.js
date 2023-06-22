e0a6de7342e8513311e99f593ebcc4120bbc0ed3
shun-is-me <rhkrtlgus@naver.com>
2023-06-22 14:20:32 +0900

    Editor change & CreateQ_page modify(2)

--- src/Pages/CreateQuestion/CreateQuestionPage.js
+++ src/Pages/CreateQuestion/CreateQuestionPage.js
@@ -5,6 +5,7 @@ import Button from "@mui/material/Button";
 import Editor from '../../Components/Editor/Editor';
 
 const CreateQuestionPage = () => {
+  
   return (
     <>
       <Header/>
@@ -29,31 +30,35 @@ const CreateQuestionPage = () => {
           <div className={style.title_div1}>Title</div>
           <div className={style.title_div2}>Be specific and imagine you’re asking a question to another person.</div>
           <div id={style.title_div3}>
-            <input type='text' id={style.title_div3_input} placeholder='e.g. Is there an R function for finding the index of an element in a vector? '/>
-          </div>
-          <div>
-            <Button
-              variant="contained"
-              sx={{
-                fontSize: 12,
-                height: "40px",
-                marginTop: "10px",
-                marginLeft: "25px",
-                marginBottom:"25px"
-              }}
-            >next
-            </Button>
+            <input type='text' className={style.title_div3_input} placeholder='e.g. Is there an R function for finding the index of an element in a vector? '/>
           </div>
         </div>
         <div className={style.contents} id={style.problem}>
           <div className={style.title_div1}>What are the details of your problem?</div>
           <div className={style.title_div2}>Introduce the problem and expand on what you put in the title. Minimum 20 characters.</div>
-          <div>
+          <div id={style.problem_div1}>
             <Editor/>
           </div>
         </div>
-        <div className={style.contents}>5</div>
-        <div className={style.contents}>6</div>
+        <div className={style.contents} id={style.tags}>
+          <div className={style.title_div1}>tags</div>
+          <div className={style.title_div2}>Add up to 5 tags to describe what your question is about. Start typing to see suggestions.</div>
+          <div id={style.tags_div}>
+            <input type='text' className={style.title_div3_input} placeholder='e.g. (angular regex django) '/>
+          </div>
+        </div>
+        <div className={style.contents}>
+          <Button
+                  variant="contained"
+                  sx={{
+                    fontSize: 12,
+                    height: "40px",
+                    marginLeft: "25px",
+                    marginBottom:"20px"
+                  }}
+                >Submit
+            </Button>
+        </div>
       </div>
     </>
   )
