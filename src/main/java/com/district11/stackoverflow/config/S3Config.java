b06b4d24e3dd285b42efb31c1f43d1cfef23248a
Parkjaeseong94 <wotjddml159@gmail.com>
2023-06-26 19:13:54 +0900

    s3 이미지 잠시 보류

--- src/main/java/com/district11/stackoverflow/config/S3Config.java
+++ src/main/java/com/district11/stackoverflow/config/S3Config.java
@@ -1,32 +1,32 @@
-package com.district11.stackoverflow.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
-
-@Configuration
-public class S3Config {
-
-    @Value("${aws.accessKey}")
-    private String accessKey;
-
-    @Value("${aws.secretKey}")
-    private String secretKey;
-
-    @Value("${aws.region}")
-    private String region;
-
-    @Bean
-    public S3Client s3Client() {
-        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
-
-        return S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(credentials))
-                .build();
-    }
-}
+//package com.district11.stackoverflow.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+//import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+//import software.amazon.awssdk.regions.Region;
+//import software.amazon.awssdk.services.s3.S3Client;
+//
+//@Configuration
+//public class S3Config {
+//
+//    @Value("${aws.accessKey}")
+//    private String accessKey;
+//
+//    @Value("${aws.secretKey}")
+//    private String secretKey;
+//
+//    @Value("${aws.region}")
+//    private String region;
+//
+//    @Bean
+//    public S3Client s3Client() {
+//        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+//
+//        return S3Client.builder()
+//                .region(Region.of(region))
+//                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+//                .build();
+//    }
+//}
