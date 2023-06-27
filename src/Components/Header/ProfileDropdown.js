8b5b05c2e58c4b351df0d054e240aaa2a510034e
shun-is-me <rhkrtlgus@naver.com>
2023-06-27 10:30:09 +0900

    드롭다운 라우팅 & 세션스토리지 id 삭제

--- src/Components/Header/ProfileDropdown.js
+++ src/Components/Header/ProfileDropdown.js
@@ -3,25 +3,30 @@ import { NavLink } from "react-router-dom";
 import Style from "./ProfileDropdown.module.css";
 import { useSetRecoilState } from "recoil";
 import { loginState } from "../../store/auth";
+import { useRecoilValue} from "recoil";
+import {userDataState } from "../../store/auth";
 
 export default function DropMenu() {
   const setLoginState = useSetRecoilState(loginState);
 
   const handleLogout = () => {
     sessionStorage.removeItem("token"); // 토큰 삭제
+    sessionStorage.removeItem("id"); //id 삭ㅔ
     setLoginState(false);
   };
+
+  const isUserData = useRecoilValue(userDataState);
 
   return (
     <>
       <div>
         <div className={Style.MenuItem}>
-          <NavLink to="/mypage" className={Style.btn}>
+          <NavLink to={`/users/${isUserData.memberId}/${isUserData.displayName}?tab=questions`} className={Style.btn}>
             My page
           </NavLink>
         </div>
         <div className={Style.MenuItem}>
-          <NavLink to="/editprofile" className={Style.btn}>
+          <NavLink to={`/users/edit/${isUserData.memberId}`} className={Style.btn}>
             Edit Profile
           </NavLink>
         </div>
